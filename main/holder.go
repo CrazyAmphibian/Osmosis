0f26da012ea084f8e9c97836336f600035cbf7a5
CrazyAmphibian <0crazyamphibian@gmail.com>
2025-05-13 13:13:18 -0700

    associations menu groundwork

--- main/holder.go
+++ main/holder.go
@@ -1,105 +1,32 @@
 components {
   id: "main"
   component: "/main/main.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 components {
   id: "gallery"
   component: "/main/gallery.gui"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 components {
   id: "image"
   component: "/main/image.gui"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 components {
   id: "tags"
   component: "/main/tags.gui"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 components {
   id: "settings"
   component: "/main/settings.gui"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 components {
   id: "implications"
   component: "/main/implications.gui"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
+}
+components {
+  id: "associations"
+  component: "/main/associations.gui"
 }
 components {
   id: "sidebar"
   component: "/main/sidebar.gui"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
